61d498bc6c51f8203a9f34b9a02848d3a0c19f76
Csongor Czezar <csongor.czezar@gmail.com>
2019-11-08 20:32:38 -0800

    Compartimentalized the project, and created the new page, called MeditationTime

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import './App.css';
+import MeditationGoal from './MeditationGoal.js';
+import MeditationTime from './MeditationTime.js';
 
 function App() {
-  const goalTimes = ["15 MINS", "30 MINS", "45 MINS", "60 MINS", "90 MINS"];
-
-  const goalTimesButton = goalTimes.map(period =>
-   <button>{period}</button>
-    );
+  
 
   return (
-     <div className="App">
-      <h1>{`What is your
-       daily meditation
-        goal?`}</h1>
-      {goalTimesButton}
-     </div>
+     <MeditationTime/>
   );
 }
 
